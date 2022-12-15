c2cce5724b6509c2cee295d2a36f68cacdd1a5c3
nancynores <86531606+nancynores@users.noreply.github.com>
2022-12-15 16:52:16 -0300

    Add files via upload

---
+++ crud/js/create.js
@@ -0,0 +1,34 @@
+function guardar() {
+ 
+    let n = document.getElementById("txtNombre").value
+    let p = parseFloat(document.getElementById("txtPrecio").value)
+    let e = document.getElementById("txtEvento").value
+    let i = document.getElementById("txtImagen").value
+ 
+    let producto = {
+        nombre: n,
+        precio: p,
+        evento: e,
+        imagen: i
+    }
+    let url = "http://nanN.pythonanywhere.com/productos"
+    var options = {
+        body: JSON.stringify(producto),
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+       // redirect: 'follow'
+    }
+    fetch(url, options)
+        .then(function () {
+            console.log("creado")
+            alert("Servicio creado con exito")
+            window.location.href = "servicios.html";   //volver a la pag servicios
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            alert("Error al crear servicio" )
+            console.error(err);
+        })
+ 
+}
